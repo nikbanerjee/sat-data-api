b9eef7755e523786644d9b2c19ac73b3b4ce754e
Andrei Vasile <andreivasile@andreis-mbp.hursley.uk.ibm.com>
2019-10-19 21:26:07 +0100

    every few frames update
    Signed-off-by: Andrei Vasile <andreivasile@andreis-mbp.hursley.uk.ibm.com>

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -11,10 +11,11 @@ module.exports = {
 
         let userLat = req.params["userLat"]
         let userLong = req.params["userLong"]
+        const userAlt = 0;
 
         const satelliteList = ["25544", "36516", "33591", "29155", "25338", "28654", "25994", "27424", "38771", "37849"];
 
-        const satPositions = await sat.getSatelliteList(satelliteList, userLat, userLong)
+        const satPositions = await sat.getSatelliteList(satelliteList, userLat, userLong, userAlt)
 
         if (satPositions === []) {
             return res.status(500).send()
